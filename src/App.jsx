61e4a20caefdbc11c933286fdd99189cd074fea4
Maikell <maikell.ods10@gmail.com>
2021-07-23 03:36:53 -0400

    Login Page Designed

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import Home from "./pages/home/Home";
 import "./app.scss";
 import Watch from "./pages/watch/Watch";
 import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 const App = () => {
-    return <Register />;
+    return <Login />;
 };
 
 export default App;
