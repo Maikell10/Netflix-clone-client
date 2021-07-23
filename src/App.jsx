9624738ba4e53724a8dd938eb1fb2db63d79af1a
Maikell <maikell.ods10@gmail.com>
2021-07-23 03:20:16 -0400

    Register Page Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import Home from "./home/Home";
+import Home from "./pages/home/Home";
 import "./app.scss";
+import Watch from "./pages/watch/Watch";
+import Register from "./pages/register/Register";
 
 const App = () => {
-    return <Home />;
+    return <Register />;
 };
 
 export default App;
