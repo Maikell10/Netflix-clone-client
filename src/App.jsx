54d92e986427d75082d46c2895a0ee27af026eca
Maikell <maikell.ods10@gmail.com>
2021-07-24 00:49:30 -0400

    React Router Dom setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,46 @@ import "./app.scss";
 import Watch from "./pages/watch/Watch";
 import Register from "./pages/register/Register";
 import Login from "./pages/login/Login";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+} from "react-router-dom";
 
 const App = () => {
-    return <Login />;
+    const user = true;
+
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact>
+                    {user ? <Home /> : <Redirect to="/register" />}
+                </Route>
+                <Route path="/register">
+                    {!user ? <Register /> : <Redirect to="/" />}
+                </Route>
+                <Route path="/login">
+                    {!user ? <Login /> : <Redirect to="/" />}
+                </Route>
+
+                {user && (
+                    <div>
+                        <Route path="/movies">
+                            <Home type="movies" />
+                        </Route>
+                        <Route path="/series">
+                            <Home type="series" />
+                        </Route>
+                        <Route path="/watch">
+                            <Watch />
+                        </Route>
+                    </div>
+                )}
+            </Switch>
+        </Router>
+    );
 };
 
 export default App;
